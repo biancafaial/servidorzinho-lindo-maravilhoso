46ba5b83adea10487f0c9689457b64efcb30d024
biancafaial <biancafaial@gmail.com>
2019-05-30 21:58:24 -0300

    adição do pacote cors e alteração de rotas usando get

--- server.js
+++ server.js
@@ -1,49 +1,16 @@
-const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const controller = require('./ComidasController')
 
-    const comidas = { pratosFavoritos: [
-        {
-          nome: "Batata frita",
-          descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-          imagem: "img/Batata-frita.jpg"
-        },
-        {
-          nome: "Macarronada",
-          descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-          imagem: "img/macarronada.jpg"
-        },
-        {
-          nome: "Falafel",
-          descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-          imagem: "img/falafel.jpg"
-        },
-        {
-          nome: "Creme de abóbora",
-          descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-          imagem: "img/creme-de-abobora.jpg"
-        },
     
-      
-] }
-    
-const servidor = http.createServer(function (request, response) {
-    if (request.url === '/') {
-        response.end('Hello Wonderful World')
-    } else if (request.url === '/comidas') {
-        if (request.method === 'GET') {
-            request.setEncoding
-            response.writeHead(200,
-                {
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin":"*"
-                })
-            response.write(JSON.stringify(comidas))
-            response.end()
-        } else if (request.method === 'POST') {
-            response.writeHead(201, { "Content-Type": "text/html;charset=utf-8" })
-            response.end("<h1> Resposta diferentão do POST </h1>")
-        }
-    }
+const servidor = express()
+servidor.use(cors())
+
+servidor.get("/comidas", (request, response)=> {
+    // response.header('Acess-Control-Allow-Origin', '*')
+    response.send(controller.getAll())
 })
+
 
 servidor.listen(3000)
 console.log('servidorzinho rodando na porta 3000')
